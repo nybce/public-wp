8870f396b499bd6516658d7da22dcaa41a10cc22
Ali Muhammad <ali@blenderbox.com>
2021-11-02 10:28:02 -0400

    Revert "TestIng Linting"
    This reverts commit bbaf15b9af236fdc61aa04d07c0dee5f7bb71fc3.

--- site/web/app/themes/boilerplate-theme/src/index.js
+++ site/web/app/themes/boilerplate-theme/src/index.js
@@ -1,20 +1,9 @@
-// import jQuery from 'jquery';
+import $ from 'jquery';
 import './sass/style.scss';
 
 // ('use strict');
-// (function test() {
-//   jQuery(() => {
-//     // console.log('Document Ready test');
-//     const a = 10;
-//     return a;
-//   })(jQuery);
-// });
-function test() {
-  const a = 10;
-  const b = 5;
-  if (a === b) {
-    return b;
-  }
-  return a;
-}
-test();
+(function () {
+	$(function () {
+		// console.log('Document Ready test');
+	});
+})($);
