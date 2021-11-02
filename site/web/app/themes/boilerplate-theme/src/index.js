84045ccf96104612e90f4ad86430485b9b6979b1
Ali Muhammad <ali@blenderbox.com>
2021-11-02 14:07:23 -0400

    TestIng Linting

--- site/web/app/themes/boilerplate-theme/src/index.js
+++ site/web/app/themes/boilerplate-theme/src/index.js
@@ -1,9 +1,11 @@
-import $ from 'jquery';
+import jQuery from 'jquery';
 import './sass/style.scss';
 
 // ('use strict');
-(function () {
-	$(function () {
-		// console.log('Document Ready test');
-	});
-})($);
+(function test() {
+  jQuery(() => {
+    // console.log('Document Ready test');
+    const a = 10;
+    return a;
+  });
+}(jQuery));
