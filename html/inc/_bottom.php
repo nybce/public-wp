9389a4b5ace24ad0d9194af74213b759b0e92da6
Adam Hochstetter <ahochstetter@blenderbox.com>
2021-11-30 05:47:29 -0500

    N2RDEV-19: linting mostly done for html. Adding fix commands to the readme

--- html/inc/_bottom.php
+++ html/inc/_bottom.php
@@ -4,11 +4,11 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 
 <!-- SCRIPTS -->
-<script src="js/jquery.min.js"></script>
-<script defer src="js/swiper.min.js"></script>
-<script defer src="js/jquery.inputmask.min.js"></script>
+<script src="js/lib/jquery.min.js"></script>
+<script defer src="js/lib/swiper.min.js"></script>
+<script defer src="js/lib/jquery.inputmask.min.js"></script>
 <!-- <script defer src="js/jquery.sumoselect.min.js"></script> -->
 <!-- <script defer src="js/jquery-ui.min.js"></script> -->
-<script defer src="js/rellax.min.js"></script>
-<script defer src="js/SmoothScroll.js"></script>
+<script defer src="js/lib/rellax.min.js"></script>
+<script defer src="js/lib/SmoothScroll.js"></script>
 <script defer src="js/global.js"></script>
--- scripts/lintCssHtml.sh
+++ scripts/lintCssHtml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "${1}" == "fix" ]]; then
-    docker-compose exec division-theme bash -c "cd /html && ../theme/node_modules/stylelint/bin/stylelint.js **/*.scss --fix"
+    docker-compose exec division-theme bash -c "cd /html/scss/ && ../../theme/node_modules/stylelint/bin/stylelint.js . --config /theme/.stylelintrc --fix"
 else
-    docker-compose exec division-theme bash -c "cd /html && ../theme/node_modules/stylelint/bin/stylelint.js **/*.scss"
+    docker-compose exec division-theme bash -c "cd /html/scss && ../../theme/node_modules/stylelint/bin/stylelint.js . --config /theme/.stylelintrc"
 fi
