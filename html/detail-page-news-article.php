67836abefbd13a6fe4a7fd21581f23148038c115
Taras <tarasb@redstone.agency>
2021-12-07 17:13:34 +0200

    add Detail Page - News Article and styles for this page

---
+++ html/detail-page-news-article.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'inc/_top.php';?>
+
+    <?php include 'inc/elements-includes/_main.php';?>
+    <?php include 'inc/flexible-includes/_section-banners.php';?>
+
+    <title>New York Blood Center:: Detail Page - News Article</title>
+</head>
+
+<body>
+    <div id="loader-wrapper"></div>
+    <div id="content-block">
+
+        <header>
+            <?php include 'inc/_header.php';?>
+        </header>
+
+        <main>
+
+            <?php include 'inc/flexible-blocks/_section-banner-type-5.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-news-article.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-contact-us-type-2.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-info-block.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-employees.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-news.php';?>
+        </main>
+
+        <footer>
+            <?php include 'inc/_footer.php';?>
+        </footer>
+
+    </div>
+
+    <?php include 'inc/elements-includes/_form-elements.php';?>
+    <?php include 'inc/elements-includes/_swiper.php';?>
+    <?php include 'inc/elements-includes/_accordion.php';?>
+
+    <?php include 'inc/flexible-includes/_section-news-article.php';?>
+    <?php include 'inc/flexible-includes/_section-contact-us.php';?>
+    <?php include 'inc/flexible-includes/_section-info-block.php';?>
+    <?php include 'inc/flexible-includes/_section-employees.php';?>
+    <?php include 'inc/flexible-includes/_section-news.php';?>
+
+    <?php include 'inc/javascripts-includes/_jquery.php';?>
+    <?php include 'inc/javascripts-includes/_swiper.php';?>
+    <?php include 'inc/javascripts-includes/_rellax.php';?>
+    <?php include 'inc/javascripts-includes/_smoothScroll.php';?>
+
+    <?php include 'inc/_bottom.php';?>
+
+    <?php include 'inc/javascripts-includes/_header.php';?>
+    <?php include 'inc/javascripts-includes/_accordion.php';?>
+    <?php include 'inc/javascripts-includes/_swiper-init.php';?>
+    <?php include 'inc/javascripts-includes/_rellax-init.php';?>
+
+</body>
+
+</html>
