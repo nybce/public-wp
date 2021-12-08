32402ad3ad3dfb9e250870818d853358e5281d3d
Adam Hochstetter <ahochstetter@blenderbox.com>
2021-12-08 14:11:26 -0500

    N2R-73 adding lb back in

--- terraform/vmachine.tf
+++ terraform/vmachine.tf
@@ -40,6 +40,35 @@ data "template_file" "init" {
   }
 }
 
+resource "azurerm_lb" "vmss" {
+  name                = "lb-${var.project}-${var.environment}"
+  location            = azurerm_resource_group.vmss.location
+  resource_group_name = azurerm_resource_group.vmss.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.vmss.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  resource_group_name = azurerm_resource_group.vmss.name
+  loadbalancer_id     = azurerm_lb.vmss.id
+  name                = "bpepool-${var.project}-${var.environment}"
+}
+
+resource "azurerm_lb_nat_pool" "lbnatpool" {
+  resource_group_name            = azurerm_resource_group.vmss.name
+  name                           = "ssh"
+  loadbalancer_id                = azurerm_lb.vmss.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 50000
+  frontend_port_end              = 50119
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+
 resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   name                = "vmachine-set-${var.project}-${var.environment}"
   location            = azurerm_resource_group.vmss.location
@@ -76,6 +105,13 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
       primary                                      = true
       subnet_id                                    = azurerm_subnet.vmss.id
       application_gateway_backend_address_pool_ids = "${azurerm_application_gateway.loadbalancer.backend_address_pool[*].id}"
+    }
+    ip_configuration {
+      name                                   = "internal-lb"
+      primary                                = false
+      subnet_id                              = azurerm_subnet.vmss.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+      load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.lbnatpool.id]
     }
   }
 
