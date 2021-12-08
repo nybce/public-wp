57f07fe6e0d8113013369aee2300df7b56427a83
Victor <viktork@redstone.agency>
2021-12-08 19:30:17 +0200

    Promo Home CTA acf block

---
+++ site/web/app/themes/nybc-theme/inc/acf/blocks/promo-home-cta.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * NYBC Promo Home CTA fields
+ *
+ * @file
+ * @package NYBC
+ */
+
+if ( function_exists( 'acf_add_local_field_group' ) ) :
+
+	acf_add_local_field_group(
+		array(
+			'key'                   => 'group_61a4bdd9ec98d',
+			'title'                 => 'Promo Home CTA',
+			'fields'                => array(
+				array(
+					'key'               => 'field_61a4bde41cd8f',
+					'label'             => 'Title',
+					'name'              => 'title',
+					'type'              => 'text',
+					'instructions'      => 'This field is required',
+					'required'          => 1,
+					'conditional_logic' => 0,
+					'wrapper'           => array(
+						'width' => '',
+						'class' => '',
+						'id'    => '',
+					),
+					'default_value'     => '',
+					'placeholder'       => '',
+					'prepend'           => '',
+					'append'            => '',
+					'maxlength'         => '',
+				),
+				array(
+					'key'               => 'field_61a4bdf51cd90',
+					'label'             => 'Body',
+					'name'              => 'body',
+					'type'              => 'text',
+					'instructions'      => 'This field is required',
+					'required'          => 1,
+					'conditional_logic' => 0,
+					'wrapper'           => array(
+						'width' => '',
+						'class' => '',
+						'id'    => '',
+					),
+					'default_value'     => '',
+					'placeholder'       => '',
+					'prepend'           => '',
+					'append'            => '',
+					'maxlength'         => '',
+				),
+				array(
+					'key'               => 'field_61a4be081cd91',
+					'label'             => 'Button',
+					'name'              => 'button',
+					'type'              => 'link',
+					'instructions'      => 'This field is required',
+					'required'          => 1,
+					'conditional_logic' => 0,
+					'wrapper'           => array(
+						'width' => '',
+						'class' => '',
+						'id'    => '',
+					),
+					'return_format'     => 'array',
+				),
+			),
+			'location'              => array(
+				array(
+					array(
+						'param'    => 'block',
+						'operator' => '==',
+						'value'    => 'acf/promo-home-cta',
+					),
+				),
+			),
+			'menu_order'            => 0,
+			'position'              => 'acf_after_title',
+			'style'                 => 'default',
+			'label_placement'       => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen'        => '',
+			'active'                => true,
+			'description'           => '',
+		)
+	);
+
+endif;
