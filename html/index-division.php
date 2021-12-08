0bac1383b57677fca352d74bbbe06ad293779698
Taras <tarasb@redstone.agency>
2021-12-08 17:58:22 +0200

    add Homepage - Division and styles for this page

---
+++ html/index-division.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'inc/_top.php';?>
+
+    <?php include 'inc/elements-includes/_main.php';?>
+    <?php include 'inc/flexible-includes/_section-banners.php';?>
+
+    <title>New York Blood Center:: Home Page</title>
+</head>
+
+<body class="division">
+    <div id="loader-wrapper"></div>
+    <div id="content-block">
+
+        <header>
+            <?php include 'inc/_header.php';?>
+        </header>
+
+        <main>
+
+            <?php include 'inc/flexible-blocks/_section-banner.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-products-and-services-type-2.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-promo.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-info-block.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-quotes.php';?>
+
+            <div class="spacer-96 spacer-xs-64"></div>
+
+            <?php include 'inc/flexible-blocks/_section-news.php';?>
+
+            <?php include 'inc/flexible-blocks/_section-employees.php';?>
+
+            <div class="spacer-120"></div>
+
+            <?php include 'inc/flexible-blocks/_section-info-block-left-right.php';?>
+
+        </main>
+
+        <footer>
+            <?php include 'inc/_footer.php';?>
+        </footer>
+
+    </div>
+
+    <?php include 'inc/elements-includes/_tabs.php';?>
+    <?php include 'inc/elements-includes/_form-elements.php';?>
+    <?php include 'inc/elements-includes/_swiper.php';?>
+
+    <?php include 'inc/flexible-includes/_section-sidebar.php';?>
+    <?php include 'inc/flexible-includes/_section-products-and-services.php';?>
+    <?php include 'inc/flexible-includes/_section-info-block.php';?>
+    <?php include 'inc/flexible-includes/_section-quotes.php';?>
+    <?php include 'inc/flexible-includes/_section-news.php';?>
+    <?php include 'inc/flexible-includes/_section-employees.php';?>
+
+    <?php include 'inc/javascripts-includes/_jquery.php';?>
+    <?php include 'inc/javascripts-includes/_swiper.php';?>
+    <?php include 'inc/javascripts-includes/_rellax.php';?>
+    <?php include 'inc/javascripts-includes/_smoothScroll.php';?>
+
+    <?php include 'inc/_bottom.php';?>
+
+    <?php include 'inc/javascripts-includes/_header.php';?>
+    <?php include 'inc/javascripts-includes/_swiper-init.php';?>
+    <?php include 'inc/javascripts-includes/_rellax-init.php';?>
+
+</body>
+
+</html>
