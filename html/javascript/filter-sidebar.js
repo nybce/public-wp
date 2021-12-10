235714133827b98d52d3830084b8b01358c0de79
Taras <tarasb@redstone.agency>
2021-12-10 11:24:12 +0200

    changed and fixed filter-sidebar.js

--- html/javascript/filter-sidebar.js
+++ html/javascript/filter-sidebar.js
@@ -1,9 +1,7 @@
-jQuery ( function ( $ ){
-
-  // filter
-  $(".filter-mobile-button").on("click", function () {
-    $(this).toggleClass("active");
-    $(this).parents('.filter-sidebar').find('.filter-sidebar-item').toggleClass('open');
-  });
-
-});
+jQuery( function( $ ) {
+    // filter
+    $( '.filter-mobile-button').on( 'click', function () {
+        $( this ).toggleClass( 'active' );
+        $( this ).parents( '.filter-sidebar' ).find( '.filter-sidebar-item' ).toggleClass( 'open' );
+    } );
+} );
