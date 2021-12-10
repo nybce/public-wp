c40a3c97c5206618968ad06026b20f85db8ed24b
Taras <tarasb@redstone.agency>
2021-12-10 11:36:11 +0200

    changed and fixed filter-sidebar.js

--- html/javascript/filter-sidebar.js
+++ html/javascript/filter-sidebar.js
@@ -1,6 +1,6 @@
 jQuery( function( $ ) {
-    // filter
-    $( '.filter-mobile-button').on( 'click', function () {
+    //  filter
+    $(  '.filter-mobile-button' ).on( 'click', function () {
         $( this ).toggleClass( 'active' );
         $( this ).parents( '.filter-sidebar' ).find( '.filter-sidebar-item' ).toggleClass( 'open' );
     } );
