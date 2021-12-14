6e91025a41bd23a1ebcb39c37fb2fe318ce98c7f
Adam Hochstetter <ahochstetter@blenderbox.com>
2021-12-14 11:18:09 -0500

    N2R-74 adding file loader, compilation configs for fonts/icons/images

--- site/web/app/themes/nybc-theme/webpack.prod.config.js
+++ site/web/app/themes/nybc-theme/webpack.prod.config.js
@@ -82,22 +82,36 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[path][name]-[hash].[ext]',
-            outputPath: '../',
-            publicPath: '/dist',
+            outputPath: '/images',
+            esModule: false
           },
         },
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
             options: {
+              esModule: false,
               name: '[name].[ext]',
               outputPath: 'fonts/'
             }
           }
         ]
+      },
+      {
+        test: /\.svg$/,
+        include: [
+          path.resolve(__dirname, './node_modules'),
+        ],
+        use: {
+          loader: 'svg-inline-loader',
+          options: {
+            outputPath: '/images/icons',
+            esModule: false
+          },
+        },
       },
 			{
 				test: /\.scss$/,
