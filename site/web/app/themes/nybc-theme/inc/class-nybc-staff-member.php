b17e7fc617e7006e4756c57a8ad536f2539f0a0f
Victor <viktork@redstone.agency>
2021-12-27 12:09:20 +0200

    Staff Detail page

--- site/web/app/themes/nybc-theme/inc/class-nybc-staff-member.php
+++ site/web/app/themes/nybc-theme/inc/class-nybc-staff-member.php
@@ -24,6 +24,8 @@ if ( ! class_exists( 'NYBC_Staff_Member' ) ) {
 		 */
 		public function __construct() {
 			add_action( 'init', array( 'NYBC_Staff_Member', 'taxonomy_post_type' ) );
+			add_filter( 'custom_permalink_before_saving', array( 'NYBC_Staff_Member', 'custom_permalink_before_saving' ), 10, 2 );
+			add_action( 'admin_head', array( 'NYBC_Staff_Member', 'admin_head' ) );
 
 			/**
 			 *  Add post type fields
@@ -35,27 +37,28 @@ if ( ! class_exists( 'NYBC_Staff_Member' ) ) {
 		 *  Register taxonomy and post types
 		 */
 		public static function taxonomy_post_type() {
+
 			register_taxonomy(
-				'staff_role',
-				array( 'staff_member' ),
+				'area_of_research',
+				array( 'staff' ),
 				array(
 					'labels'             => array(
-						'name'              => esc_html__( 'Staff role', 'nybc' ),
-						'singular_name'     => esc_html__( 'Staff role', 'nybc' ),
-						'search_items'      => esc_html__( 'Search Staff roles', 'nybc' ),
-						'all_items'         => esc_html__( 'All Staff roles', 'nybc' ),
-						'view_item '        => esc_html__( 'View Staff role', 'nybc' ),
-						'parent_item'       => esc_html__( 'Parent Staff role', 'nybc' ),
-						'parent_item_colon' => esc_html__( 'Parent Staff role:', 'nybc' ),
-						'edit_item'         => esc_html__( 'Edit Staff role', 'nybc' ),
-						'update_item'       => esc_html__( 'Update Staff role', 'nybc' ),
-						'add_new_item'      => esc_html__( 'Add Staff role', 'nybc' ),
-						'new_item_name'     => esc_html__( 'New Staff role', 'nybc' ),
-						'menu_name'         => esc_html__( 'Staff role', 'nybc' ),
+						'name'              => esc_html__( 'Area of Research', 'nybc' ),
+						'singular_name'     => esc_html__( 'Area of Research', 'nybc' ),
+						'search_items'      => esc_html__( 'Search Area of Research', 'nybc' ),
+						'all_items'         => esc_html__( 'All Areas of Research', 'nybc' ),
+						'view_item '        => esc_html__( 'View Area of Research', 'nybc' ),
+						'parent_item'       => esc_html__( 'Parent Area of Research', 'nybc' ),
+						'parent_item_colon' => esc_html__( 'Parent Area of Research:', 'nybc' ),
+						'edit_item'         => esc_html__( 'Edit Area of Research', 'nybc' ),
+						'update_item'       => esc_html__( 'Update Area of Research', 'nybc' ),
+						'add_new_item'      => esc_html__( 'Add Area of Research', 'nybc' ),
+						'new_item_name'     => esc_html__( 'New Area of Research', 'nybc' ),
+						'menu_name'         => esc_html__( 'Area of Research', 'nybc' ),
 					),
 					'hierarchical'       => true,
 					'query_var'          => true,
-					'publicly_queryable' => true,
+					'publicly_queryable' => false,
 					'public'             => true,
 					'show_admin_column'  => true,
 					'show_in_nav_menus'  => true,
@@ -65,36 +68,78 @@ if ( ! class_exists( 'NYBC_Staff_Member' ) ) {
 				)
 			);
 
-			register_post_type(
-				'staff_member',
-				array(
-					'labels'            => array(
-						'name'               => esc_html__( 'Staff Members', 'nybc' ),
-						'singular_name'      => esc_html__( 'Staff Member', 'nybc' ),
-						'add_new'            => esc_html__( 'Add Staff Member', 'nybc' ),
-						'add_new_item'       => esc_html__( 'Add Staff Member', 'nybc' ),
-						'edit_item'          => esc_html__( 'Edit Staff Member', 'nybc' ),
-						'new_item'           => esc_html__( 'New Staff Member', 'nybc' ),
-						'view_item'          => esc_html__( 'View Staff Member', 'nybc' ),
-						'search_items'       => esc_html__( 'Search Staff Member', 'nybc' ),
-						'not_found'          => esc_html__( 'Staff Member not found', 'nybc' ),
-						'not_found_in_trash' => esc_html__( 'Staff Member not found in trash', 'nybc' ),
-						'parent_item_colon'  => esc_html__( 'Staff Member', 'nybc' ),
-						'menu_name'          => esc_html__( 'Staff Members', 'nybc' ),
-					),
-					'show_in_nav_menus' => true,
-					'show_ui'           => true,
-					'public'            => true,
-					'show_in_rest'      => false,
-					'menu_position'     => 20,
-					'supports'          => array( 'title', 'editor', 'thumbnail' ),
-					'menu_icon'         => null,
-					'has_archive'       => false,
-					'taxonomies'        => array( 'post_tag', 'staff_role' ),
-				)
-			);
+				register_post_type(
+					'staff',
+					array(
+						'labels'            => array(
+							'name'               => esc_html__( 'Staff Members', 'nybc' ),
+							'singular_name'      => esc_html__( 'Staff Member', 'nybc' ),
+							'add_new'            => esc_html__( 'Add Staff Member', 'nybc' ),
+							'add_new_item'       => esc_html__( 'Add Staff Member', 'nybc' ),
+							'edit_item'          => esc_html__( 'Edit Staff Member', 'nybc' ),
+							'new_item'           => esc_html__( 'New Staff Member', 'nybc' ),
+							'view_item'          => esc_html__( 'View Staff Member', 'nybc' ),
+							'search_items'       => esc_html__( 'Search Staff Member', 'nybc' ),
+							'not_found'          => esc_html__( 'Staff Member not found', 'nybc' ),
+							'not_found_in_trash' => esc_html__( 'Staff Member not found in trash', 'nybc' ),
+							'parent_item_colon'  => esc_html__( 'Staff Member', 'nybc' ),
+							'menu_name'          => esc_html__( 'Staff Members', 'nybc' ),
+						),
+						'show_in_nav_menus' => true,
+						'show_ui'           => true,
+						'public'            => true,
+						'show_in_rest'      => false,
+						'menu_position'     => 20,
+						'supports'          => array( 'title', 'editor', 'thumbnail' ),
+						'menu_icon'         => null,
+						'has_archive'       => false,
+						'taxonomies'        => array( 'area_of_research' ),
+					)
+				);
 
 		}
+
+		/**
+		 *  Modify permalink
+		 *
+		 * @param string  $permalink post permalink.
+		 * @param integer $post_id post ID.
+		 *
+		 * @return string
+		 */
+		public static function custom_permalink_before_saving( $permalink, $post_id ) {
+			$post = get_post( $post_id );
+			if ( 'staff' === $post->post_type ) {
+				$parent = get_field( 'parent_page' );
+				if ( ! empty( $parent ) ) {
+					$parent_link = get_the_permalink( $parent );
+					$parent_link = str_replace( NYBC_HOME_URI, '', $parent_link );
+					$permalink   = $parent_link . $post->post_name . '/';
+				}
+			}
+
+			return $permalink;
+		}
+
+		/**
+		 *  Insert admin scripts
+		 */
+		public static function admin_head() {
+			global $pagenow;
+			if ( 'post.php' !== $pagenow && 'post-new.php' !== $pagenow ) {
+				return;
+			}
+			?>
+			<script>
+				jQuery(function ($) {
+					$('div[data-name="parent_page"] select').on('change', function () {
+						$('#custom-permalinks-post-slug').val('');
+					});
+				});
+			</script>
+			<?php
+		}
+
 	}
 
 	new NYBC_Staff_Member();
---
+++ site/web/app/themes/nybc-theme/template-parts/recent-news.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Staff Recent News
+ *
+ * @package NYBC
+ */
+
+$news = get_posts(
+	array(
+		'post_type'   => 'post',
+		'numberposts' => 3,
+		'fields'      => 'ids',
+	)
+);
+
+if ( empty( $news ) ) {
+	return;
+}
+
+$blog_link = ! empty( NYBC_NEWS_PAGE_ID ) ? get_the_permalink( NYBC_NEWS_PAGE_ID ) : '';
+?>
+<div class="spacer-96 spacer-xs-64"></div>
+<div class="section news">
+
+	<div class="container">
+
+		<div class="row">
+			<div class="col-12 d-flex justify-content-between">
+				<div class="title-wrapper">
+					<div class="h4 title fw-800">
+						<?php esc_html_e( 'Recent News', 'nybc' ); ?>
+					</div>
+					<?php if ( $blog_link ) { ?>
+						<a href="<?php echo esc_url( $blog_link ); ?>" class="btn-link btn-link-primary right"><?php esc_html_e( 'View All', 'nybc' ); ?></a>
+					<?php } ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="spacer-48 spacer-xs-24"></div>
+
+		<div class="swiper-entry news-swiper">
+
+			<div class="swiper-container"
+				data-options='{"slidesPerView":3, "autoHeight": true, "spaceBetween": 24, "breakpoints":{"991":{"slidesPerView": 2}, "767":{"slidesPerView": 1, "spaceBetween": 16}}}'>
+				<div class="swiper-wrapper">
+					<?php
+					foreach ( $news as $news_id ) {
+						$image_url   = get_the_post_thumbnail_url( $news_id, 'full' );
+						$block_title = get_the_title( $news_id );
+						$date        = get_the_time( 'M j, Y', $news_id );
+						$tags        = get_the_tags( $news_id );
+						?>
+						<div class="swiper-slide">
+							<a href="<?php echo esc_url( get_the_permalink( $news_id ) ); ?>" class="news-item">
+								<div class="news-img">
+									<?php NYBC_Helpers::picture( $image_url, $block_title, 517, 292, true ); ?>
+								</div>
+								<div class="border-top"></div>
+								<div class="news-info">
+									<?php
+									if ( ! empty( $tags ) ) {
+										?>
+										<ul class="tags-list">
+											<?php foreach ( $tags as $tg ) { ?>
+												<li><?php echo esc_html( $tg->name ); ?></li>
+											<?php } ?>
+										</ul>
+									<?php } ?>
+									<div class="spacer-16"></div>
+
+									<div class="h5 title fw-800"><?php echo esc_html( $block_title ); ?></div>
+
+									<div class="spacer-16"></div>
+
+									<div class="date"><?php echo esc_html( $date ); ?></div>
+								</div>
+							</a>
+						</div>
+					<?php } ?>
+
+				</div>
+			</div>
+			<div class="spacer-xs-16"></div>
+			<div class="swiper-button-wrapper">
+				<div class="swiper-button-prev"><i></i></div>
+				<div class="swiper-pagination"></div>
+				<div class="swiper-button-next"><i></i></div>
+			</div>
+
+		</div>
+	</div>
+	<div class="spacer-120 spacer-xs-64"></div>
+</div>
