7e968a1c2fcf4d2f695fbae9c5bd47b6b3976f7b
Victor <viktork@redstone.agency>
2021-12-30 19:41:16 +0200

    lint js

--- site/web/app/themes/nybc-theme/src/js/sumoselect-init.js
+++ site/web/app/themes/nybc-theme/src/js/sumoselect-init.js
@@ -1,13 +1,13 @@
 import './lib/jquery.sumoselect.min';
 
-jQuery(function ($) {
-  // sumoselect
-  if ($('.SelectBox').length) {
-    $('.SelectBox').each(function () {
-      $(this).SumoSelect({
-        floatWidth: 0,
-        nativeOnDevice: [],
-      });
-    });
-  }
-});
+jQuery( function( $ ) {
+	// sumoselect
+	if ( $( '.SelectBox' ).length ) {
+		$( '.SelectBox' ).each( function() {
+			$( this ).SumoSelect( {
+				floatWidth: 0,
+				nativeOnDevice: [],
+			} );
+		} );
+	}
+} );
