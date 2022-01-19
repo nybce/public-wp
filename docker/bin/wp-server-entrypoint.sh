6685a48d9c33040f5ff9d7585b9bfc535037defc
Adam Hochstetter <ahochstetter@blenderbox.com>
2022-01-19 16:40:16 -0500

    updating port and vault

--- docker/bin/wp-server-entrypoint.sh
+++ docker/bin/wp-server-entrypoint.sh
@@ -3,7 +3,7 @@ set -a
 echo "upgrade composer dependencies"
 composer update -vvv -n
 chmod 777 /envs/staging.env
-ansible-vault decrypt /envs/staging.env --vault-password-file=/.vaultpass
+ansible-vault decrypt /envs/staging.env --vault-password-file=/.vaultpass-test
 source /envs/staging.env
 cp /envs/staging.env /site/.env
 composer install -n
