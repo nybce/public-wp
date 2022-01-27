3d717f8bc9fafe3caaabdd73510bdd757af061fb
Adam Hochstetter <ahochstetter@blenderbox.com>
2022-01-27 16:00:43 -0500

    running as root

--- docker/bin/wp-server-entrypoint.sh
+++ docker/bin/wp-server-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -a
 bash /echo_ansible_vault_pass.sh > /envs/.vaultpass
-ansible-vault decrypt /envs/dev.env --vault-password-file=/echo_ansible_vault_pass.sh
-source /envs/dev.env
-cp /envs/dev.env /site/.env
+ansible-vault decrypt /site/.env --vault-password-file=/echo_ansible_vault_pass.sh
+source /site/.env
 exec "$@"
