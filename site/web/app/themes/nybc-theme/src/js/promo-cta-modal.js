2b89794e102766ba5077ed3cdfbf636c7b52b114
Victor <viktork@redstone.agency>
2022-03-04 11:34:48 +0200

    Archive Sidebar Filter by Topics should filter by Tags fix, Set Letter-spacing to 0, Carousel - Callout with CTA fix, Main navigation fix, Full Width Feature CTA Carousel auto rotate, Article Banner Header Bottom Margin, Promo popup, Full Width Pullquote

--- site/web/app/themes/nybc-theme/src/js/promo-cta-modal.js
+++ site/web/app/themes/nybc-theme/src/js/promo-cta-modal.js
@@ -1,10 +1,47 @@
-jQuery( function( $ ) {
-	// promo-cta-modal
-	setTimeout( function() {
-		$( '.promo-cta-modal' ).addClass( 'active' );
-	}, 4000 );
+jQuery(function ($) {
+  // Cookies
+  function createCookie(name, value, days) {
+    let expires;
 
-	$( document ).on( 'click', '.promo-cta-modal .btn-close', function() {
-		$( '.promo-cta-modal' ).removeClass( 'active' );
-	} );
-} );
+    if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = '; expires=' + date.toGMTString();
+    } else {
+      expires = '';
+    }
+    document.cookie =
+      encodeURIComponent(name) +
+      '=' +
+      encodeURIComponent(value) +
+      expires +
+      '; path=/';
+  }
+
+  function readCookie(name) {
+    const nameEQ = encodeURIComponent(name) + '=';
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1, c.length);
+      }
+      if (c.indexOf(nameEQ) === 0) {
+        return decodeURIComponent(c.substring(nameEQ.length, c.length));
+      }
+    }
+    return null;
+  }
+
+  // promo-cta-modal
+  if (!readCookie('nybc-promo-showed') && $('.promo-cta-modal').length) {
+    setTimeout(function () {
+      $('.promo-cta-modal').addClass('active');
+      createCookie('nybc-promo-showed', 1);
+    }, 4000);
+  }
+
+  $(document).on('click', '.promo-cta-modal .btn-close', function () {
+    $(this).parents('.promo-cta-modal').removeClass('active');
+  });
+});
--- site/web/app/themes/nybc-theme/src/js/video.js
+++ site/web/app/themes/nybc-theme/src/js/video.js
@@ -1,31 +1,31 @@
-jQuery( function( $ ) {
-	$( document ).ready( function() {
-		$( 'a.play-video' ).click( function() {
-			$( this ).parents( '.video-container' ).find( 'video' ).trigger( 'play' );
-			$( this ).hide();
-			$( this ).next( 'a.pause-video' ).show();
-			// IF VIDEO HAS COVER
-			const hasitcover = $( this )
-				.parents( '.video-container' )
-				.find( '.video-cover' );
-			if ( hasitcover.length > 0 ) {
-				hasitcover.fadeOut();
-			}
-		} );
-		$( 'a.pause-video' ).click( function() {
-			$( this ).parents( '.video-container' ).find( 'video' ).trigger( 'pause' );
-			$( this ).hide();
-			$( this ).prev( 'a.play-video' ).show();
-		} );
-		$( '.video-item video' ).on( 'ended', function() {
-			const hasitcovertwo = $( this )
-				.parents( '.video-container' )
-				.find( '.video-cover' );
-			if ( hasitcovertwo.length > 0 ) {
-				hasitcovertwo.fadeIn();
-			}
-			$( this ).parents( '.video-container' ).find( 'a.play-video' ).show();
-			$( this ).parents( '.video-container' ).find( 'a.pause-video' ).hide();
-		} );
-	} );
-} );
+jQuery(function ($) {
+  $(document).ready(function () {
+    $('a.play-video').click(function () {
+      $(this).parents('.video-container').find('video').trigger('play');
+      $(this).hide();
+      $(this).next('a.pause-video').show();
+      // IF VIDEO HAS COVER
+      const hasitcover = $(this)
+        .parents('.video-container')
+        .find('.video-cover');
+      if (hasitcover.length > 0) {
+        hasitcover.fadeOut();
+      }
+    });
+    $('a.pause-video').click(function () {
+      $(this).parents('.video-container').find('video').trigger('pause');
+      $(this).hide();
+      $(this).prev('a.play-video').show();
+    });
+    $('.video-item video').on('ended', function () {
+      const hasitcovertwo = $(this)
+        .parents('.video-container')
+        .find('.video-cover');
+      if (hasitcovertwo.length > 0) {
+        hasitcovertwo.fadeIn();
+      }
+      $(this).parents('.video-container').find('a.play-video').show();
+      $(this).parents('.video-container').find('a.pause-video').hide();
+    });
+  });
+});
