6cc61d858d2912be76fcef9bebd670eff03674b4
Victor <viktork@redstone.agency>
2022-04-20 14:38:50 +0300

    lint fixes

--- site/web/app/themes/nybc-theme/src/js/accordion.js
+++ site/web/app/themes/nybc-theme/src/js/accordion.js
@@ -1,21 +1,21 @@
-jQuery(function ($) {
-  // accordion
-  $(document).on(
-    'click',
-    '.accordion:not(.employment-accord) .accordion-item .accordion-title',
-    function () {
-      if ($(this).hasClass('active')) {
-        $(this).removeClass('active').next().slideUp();
-      } else {
-        $(this)
-          .closest('.accordion')
-          .find('.accordion-title')
-          .not(this)
-          .removeClass('active')
-          .next()
-          .slideUp();
-        $(this).addClass('active').next().slideDown();
-      }
-    }
-  );
-});
+jQuery( function( $ ) {
+	// accordion
+	$( document ).on(
+		'click',
+		'.accordion:not(.employment-accord) .accordion-item .accordion-title',
+		function() {
+			if ( $( this ).hasClass( 'active' ) ) {
+				$( this ).removeClass( 'active' ).next().slideUp();
+			} else {
+				$( this )
+					.closest( '.accordion' )
+					.find( '.accordion-title' )
+					.not( this )
+					.removeClass( 'active' )
+					.next()
+					.slideUp();
+				$( this ).addClass( 'active' ).next().slideDown();
+			}
+		}
+	);
+} );
