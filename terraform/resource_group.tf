825fa2f97bba18ed776cbfa57511521a3abf9f73
George Griffin <ggriffin@blenderbox.com>
2022-09-01 15:19:11 -0400

    Update terraform to avoid creating new ACR

--- terraform/resource_group.tf
+++ terraform/resource_group.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "vmss" {
 }
 
 resource "azurerm_role_assignment" "access" {
-  scope                = "${azurerm_resource_group.vmss.id}"
+  scope                = azurerm_resource_group.vmss.id
   role_definition_name = "Contributor"
   principal_id         = "0e57e1d8-8d66-45f4-9455-fc584b103c3f"
 }
