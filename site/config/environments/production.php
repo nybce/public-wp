7a8e57d19c1bd6d2689720b8ed5df1aa05597841
George Griffin <ggriffin@blenderbox.com>
2022-09-01 17:02:56 -0400

    Add production env

---
+++ site/config/environments/production.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Configuration overrides for WP_ENV === 'development'
+ */
+
+use Roots\WPConfig\Config;
+use function Env\env;
+
+Config::define('SAVEQUERIES', false);
+Config::define('WP_DEBUG', false);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('SCRIPT_DEBUG', false);
+
+ini_set('display_errors', 0);
+
+// Enable plugin and theme updates and installation from the admin
+Config::define( 'FS_METHOD', 'direct' );
+Config::define('DISALLOW_FILE_MODS', true);
+
+define('MICROSOFT_AZURE_ACCOUNT_NAME', env('MICROSOFT_AZURE_ACCOUNT_NAME'));
+define('MICROSOFT_AZURE_ACCOUNT_KEY', env('MICROSOFT_AZURE_ACCOUNT_KEY'));
+define('MICROSOFT_AZURE_USE_FOR_DEFAULT_UPLOAD', 1);
+
