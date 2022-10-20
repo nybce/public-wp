d5c50e4b0f098301ad4b72381807328062d7e6cf
George Griffin <ggriffin@blenderbox.com>
2022-10-20 16:15:45 -0400

    Update config files

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -13,3 +13,12 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('WP_DEBUG', true);`
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
+
+// Enable plugin and theme updates and installation from the admin
+Config::define( 'FS_METHOD', 'direct' );
+Config::define('DISALLOW_FILE_MODS', true);
+
+define('MICROSOFT_AZURE_ACCOUNT_NAME', env('MICROSOFT_AZURE_ACCOUNT_NAME'));
+define('MICROSOFT_AZURE_ACCOUNT_KEY', env('MICROSOFT_AZURE_ACCOUNT_KEY'));
+define('MICROSOFT_AZURE_USE_FOR_DEFAULT_UPLOAD', 1);
+define('MICROSOFT_AZURE_CONTAINER', env('MICROSOFT_AZURE_STORAGES_CONTAINER'));
