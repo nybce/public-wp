b3838d84da909bc3a86833f0b20db3d6a31b35d9
Jeff Chang <tsavij@gmail.com>
2022-12-12 16:12:20 -0500

    Revert "third tier styling p2 and js hover function"
    This reverts commit b296978b173ca0f96f42b65532742c25c69c37ce.

--- site/web/app/themes/nybc-theme/src/js/header.js
+++ site/web/app/themes/nybc-theme/src/js/header.js
@@ -25,14 +25,4 @@ jQuery( function( $ ) {
 				.removeClass( 'remove-overflow' );
 		}
 	} );
-
-	$( '.dropdown-v2-item.has-children' ).on( 'hover', function() {
-		var menuid = $(this).data('menuid')
-		var target_submenu = '.submenu-' . menuid;
-		console.log('hit');
-		console.log(menuid);
-		console.log(target_submenu);
-		$(target_submenu).toggleClass('visible');
-
-	})
 } );
