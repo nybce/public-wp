88e687572b1cfae9afd817d0c4e58f00b1725c60
Jeff Chang <tsavij@gmail.com>
2023-01-17 11:12:20 -0500

    fix for 1200 to 1300 mobile menu

--- site/web/app/themes/nybc-theme/src/js/header.js
+++ site/web/app/themes/nybc-theme/src/js/header.js
@@ -10,14 +10,14 @@ jQuery( function( $ ) {
 
 	// mobile menu
 	$( document ).on( 'click', '.dropdown-btn', function() {
-		if ( $( window ).width() < 1199 ) {
+		if ( $( window ).width() < 1299 ) {
 			$( this ).parent().addClass( 'active' );
 			$( this ).parents().find( '.toggle-block.open' ).addClass( 'remove-overflow' );
 		}
 	} );
 
 	$( '.dropdown-close' ).on( 'click', function() {
-		if ( $( window ).width() < 1199 ) {
+		if ( $( window ).width() < 1299 ) {
 			$( this ).parents( '.dropdown-item' ).removeClass( 'active' );
 			$( this )
 				.parents()
