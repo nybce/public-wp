a1531bb4ef874c12a5738ca8521794aad03860de
RJ Shoemaker <raymond.shoemaker@spigroup.com>
2023-08-22 12:42:18 -0400

    centering portfolio logos

--- site/web/app/themes/nybc-venture/page-portfolio.php
+++ site/web/app/themes/nybc-venture/page-portfolio.php
@@ -11,17 +11,17 @@ get_header();
 ?>
 <div class="page-wrapper portfolio-page">
 
-<div class="nybcv-wrapper">
+	<div class="nybcv-wrapper">
 
-  <div class="container">
-	<div class="nybcv-block">
-		<div class="nybcv-block--intro">
-			<h3><?php echo get_field('title')?></h3>
-			<?php echo get_field('introduction_text') ?>
-		</div>
+		<div class="container">
+			<div class="nybcv-block">
+				<div class="nybcv-block--intro">
+					<h3><?php echo get_field('title')?></h3>
+					<?php echo get_field('introduction_text') ?>
+				</div>
 
-<div class="portfolio-grid">
-		<?php
+				<div class="portfolio-grid">
+					<?php
 
 if( have_rows('portfolio') ):
 
@@ -32,23 +32,23 @@ if( have_rows('portfolio') ):
     $description = get_sub_field('description');
     $link = get_sub_field('company_link');
 ?>
-	<div class="portfolio-grid--square">
-		<div class="portfolio-grid--square--logo">
-			<?php if($logo): ?>
-				<img src="<?php echo $logo['sizes']['large'] ?>" alt="<?php echo $logo['alt'] ?>"/>
-			<? else: ?>
-				<h2><?php echo $company_title ?></h2>
-			<?php endif;?>
-		</div>
-		<div class="portfolio-grid--square--intro">
-			<h4><?php echo $company_title ?></h4>
-			<p><?php echo $description ?></p>
-			<?php if($link): ?>
-				<a href="<?php echo $link['url'] ?>" target="_blank"><?php echo $link['title'] ?></a>
-			<?php endif; ?>
-		</div>
-	</div>
-<?php
+					<div class="portfolio-grid--square">
+						<div class="portfolio-grid--square--logo">
+							<?php if($logo): ?>
+							<img src="<?php echo $logo['sizes']['large'] ?>" alt="<?php echo $logo['alt'] ?>" />
+							<? else: ?>
+							<h2><?php echo $company_title ?></h2>
+							<?php endif;?>
+						</div>
+						<div class="portfolio-grid--square--intro">
+							<h4><?php echo $company_title ?></h4>
+							<p><?php echo $description ?></p>
+							<?php if($link): ?>
+							<a href="<?php echo $link['url'] ?>" target="_blank"><?php echo $link['title'] ?></a>
+							<?php endif; ?>
+						</div>
+					</div>
+					<?php
   endwhile;
 
 // No value.
@@ -56,10 +56,10 @@ else :
     // Do something...
 endif;
 		 ?>
-</div>
+				</div>
+			</div>
+		</div>
 	</div>
-</div>
-</div>
 
 
 </div>
