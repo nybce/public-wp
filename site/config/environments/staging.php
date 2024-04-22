190c19d40d83150d1554ebfc3a0ce836a77580a0
Cinthya Rivas <cinthya.rivas@ruderfinn.com>
2024-04-22 14:05:41 -0400

    updated config file for dev and staging

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -14,6 +14,12 @@ use function Env\env;
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
 
+ Config::define('SAVEQUERIES', true);
+ Config::define('WP_DEBUG', true);
+ Config::define('WP_DEBUG_DISPLAY', true);
+ Config::define('WP_DEBUG_LOG', true);
+ Config::define('SCRIPT_DEBUG', true);
+
 // Enable plugin and theme updates and installation from the admin
 Config::define( 'FS_METHOD', 'direct' );
 Config::define('DISALLOW_FILE_MODS', false);
