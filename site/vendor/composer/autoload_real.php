fae2107a2ab544a4a4fed1fe33ab1d831a1f73d1
Cinthya Rivas <cinthya.rivas@ruderfinn.com>
2024-05-01 12:29:28 -0400

    update local docker file

--- site/vendor/composer/autoload_real.php
+++ site/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit0c5982e77bd45146bfa537ad2f88b546
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit0c5982e77bd45146bfa537ad2f88b546', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit0c5982e77bd45146bfa537ad2f88b546', 'loadClassLoader'));
